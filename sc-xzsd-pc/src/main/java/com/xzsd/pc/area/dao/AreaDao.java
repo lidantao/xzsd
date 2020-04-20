ed681e776ffe45df7b0a431167bfbaf58ca9af8c
lidantao <1114580567@qq.com>
2020-04-20 17:07:12 +0800

    sc-xzsd-pc

---
+++ sc-xzsd-pc/src/main/java/com/xzsd/pc/area/dao/AreaDao.java
@@ -0,0 +1,25 @@
+package com.xzsd.pc.area.dao;
+
+
+/**
+ * 作者：李丹涛
+ * 时间：2020/04/13晚上22：27分
+ * 功能：查询省市区下拉框Dao接口
+ */
+
+import com.xzsd.pc.area.entity.AreaInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AreaDao {
+
+    /**
+     * 作者：李丹涛
+     * 时间：2020/04/13晚上22：27分
+     * 功能：查询省市区下拉框接口
+     * areaCode：区域编号（省传0，市传省编号，区传市编号）
+     */
+    List<AreaInfo> listArea(String parentCode);
+}

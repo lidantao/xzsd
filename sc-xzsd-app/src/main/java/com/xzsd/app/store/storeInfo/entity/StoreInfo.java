b73d41498502708c14e7ef290f8154b68d299303
lidantao <1114580567@qq.com>
2020-04-28 14:16:25 +0800

    sc-xzsd-app

---
+++ sc-xzsd-app/src/main/java/com/xzsd/app/store/storeInfo/entity/StoreInfo.java
@@ -0,0 +1,51 @@
+package com.xzsd.app.store.storeInfo.entity;
+
+
+/**
+ * 作者：李丹涛
+ * 时间：2020/04/15下午14：47分
+ * 功能：店长个人信息之查询司机信息管理实体类信息
+ */
+
+
+public class StoreInfo {
+
+    /**
+     * 用户编号
+     */
+    private String userCode;
+
+    /**
+     * 用户名称
+     */
+    private String userName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+}
